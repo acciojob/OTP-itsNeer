09bdd26cacc1deb49561c8944220913210b1ffb9
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-09-24 15:55:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let Code= document.querySelector(".code");
+Code.forEach((input ,index)=>{
+	input.addEventListener("input", ()=>{
+		if(input.value.length==1 && index <= input.length)
+	{
+	 Code[index+1].focus();
+	}
+
+		if(input.value=="" && index>0)
+		{
+			Code[index-1].focus();
+		}
+})
+	input.addEventListener("keydown", (e)=>{
+		if(e.key=="Backspace" && input.value="" && index>0)
+		{
+			Code[index-1].focus();
+		}
+	})
+})
